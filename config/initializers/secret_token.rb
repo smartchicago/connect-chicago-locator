da343e499e6371981c8fc7d969ae8bd03bf976ec
Derek Eder <derek.eder@gmail.com>
2012-08-27 10:54:01 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TechLocator::Application.config.secret_token = '14fc65fcf2c8d6797dc2a67b82f17d10fbce20764e30c73d9607f3d9bf0e0d2ff801e14413ad028c2613fdd0041bcc9bb37d149ac99f0e47d3b185ddb62d1fd4'
