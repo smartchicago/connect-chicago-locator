53a8203a7b97fc177e4607ee1400de988b27d9d4
Derek Eder <derek.eder@gmail.com>
2012-08-27 14:29:33 -0500

    minor fix for config

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -2,6 +2,7 @@ begin
   APP_CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env.to_s]
 rescue Errno::ENOENT
   puts "config file not found. loading from ENV"
+  APP_CONFIG = []
   APP_CONFIG['fusion_table_id'] = ENV['google_account']
   APP_CONFIG['google_account'] = ENV['google_account']
   APP_CONFIG['google_password'] = ENV['google_password'] 
@@ -10,8 +11,8 @@ rescue Errno::ENOENT
   APP_CONFIG['flickr_secret'] = ENV['flickr_secret'] 
 end
 
-#puts "google_account: #{google_account}"
-#puts "google_password: #{google_password}"
+puts "google_account: #{google_account}"
+puts "google_password: #{google_password}"
 
 #initialize Fusion Tables API
 FT = GData::Client::FusionTables.new      
