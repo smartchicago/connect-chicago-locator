c52d153f6ea043bb1efbb05baca20160d6a7ec29
Derek Eder <derek.eder@gmail.com>
2012-08-27 15:06:15 -0500

    added pass protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate
+  
+  protected
+  def authenticate
+    if Rails.env.production?
+      authenticate_or_request_with_http_basic do |username, password|
+        username == ENV['AUTH_USERNAME'] && password == ENV['AUTH_PASS']
+      end
+    end
+  end
 end
