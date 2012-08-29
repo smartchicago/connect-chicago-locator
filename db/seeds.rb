f4c660d0a4c7ef6054f17b79fcd22e901f753ac4
Derek Eder <derek.eder@gmail.com>
2012-08-29 15:21:36 -0500

    fix for attr_accessible for new admin columns

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 puts 'SETTING UP ADMIN USERS'
-admin1 = Admin.create! :email => 'test@example.com', :password => 'testpass', :password_confirmation => 'testpass', :approved => true, :superadmin => true
+admin1 = Admin.create! :email => 'test@example.com', :password => 'testpass', :password_confirmation => 'testpass'
+admin1.approved = true
+admin1.superadmin = true
+admin1.save!
 puts 'New user created: ' << admin1.email
