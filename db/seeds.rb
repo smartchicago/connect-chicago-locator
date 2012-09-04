2ae4037cb6c10056c12d36e731e608cf649aa606
Derek Eder <derek.eder@gmail.com>
2012-09-04 16:30:48 -0500

    fix for setting user/pass admin seeds from env

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,8 +12,8 @@ admin1Pass = 'testpass'
 
 if (Rails.env.production?)
 	puts 'In production, using ENV credentials'
-	admin1Email = ENV['admin1Email']
-	admin1Pass = ENV['admin1Pass']
+	admin1Email = ENV['admin1Email'].dup
+	admin1Pass = ENV['admin1Pass'].dup
 end
 
 admin1 = Admin.create! :email => admin1Email, :password => admin1Pass, :password_confirmation => admin1Pass
