c4274870c0a1ced37b5b1e9676812b1a60b0a11c
Derek Eder <derek.eder@gmail.com>
2012-10-22 18:32:52 -0500

    removed debug comments from location controller

--- app/controllers/location_controller.rb
+++ app/controllers/location_controller.rb
@@ -49,9 +49,6 @@ class LocationController < ApplicationController
     location_edit = FT.execute("SELECT * FROM #{APP_CONFIG['fusion_table_id']} WHERE slug = '#{params[:id]}';").first
     @location_title = location_edit[:organization_name]
 
-    # puts 'parameters'
-    # puts params[:location]
-
     # stuff in new values from the form in to the Fusion Table hash object
     require 'json'
     change = {}
@@ -100,9 +97,6 @@ class LocationController < ApplicationController
         @location_changes.save
       end
 
-      # puts "new location data"
-      # puts location_edit.inspect
-
       table = GData::Client::FusionTables::Table.new(FT, :table_id => APP_CONFIG['fusion_table_id'], :name => "My table")
       row_id = FT.execute("SELECT ROWID FROM #{APP_CONFIG['fusion_table_id']} WHERE slug = '#{params[:id]}';").first[:rowid]
       table.update row_id, location_edit
@@ -110,7 +104,6 @@ class LocationController < ApplicationController
       flash[:notice] = "Location saved successfully!"
       redirect_to "/location/#{params[:id]}"
     else
-      #puts @location.errors.messages.inspect
       render :action => 'edit'
     end
   end
