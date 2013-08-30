cb15e970c56605ad0ad782f21fd99ba4f194c567
keithgordon <keithg07@gmail.com>
2013-08-30 17:11:08 -0500

    fixed flickr issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,11 @@
 puts 'SETTING UP ADMIN USERS'
 
-admin1Email = 'test@example.com'
-admin1Pass = 'testpass'
+admin1Email = 'derek.eder@gmail.com'
+admin1Pass = 'smartchicago'
 admin1First = "Test"
 admin1Last = "User"
 admin1Org = "Smart Chicago"
+
 
 if (Rails.env.production?)
 	puts 'In production, using ENV credentials'
@@ -12,7 +13,8 @@ if (Rails.env.production?)
 	admin1Pass = ENV['admin1Pass'].dup
 end
 
-admin1 = Admin.create! :first_name => admin1First, :last_name => admin1Last, :organization => admin1Org,  :email => admin1Email, :password => admin1Pass, :password_confirmation => admin1Pass
+admin1 = Admin.create! :first_name => admin1First, :last_name => admin1Last, :organization => admin1Org,  :email => admin1Email, :password => admin1Pass, :password_confirmation => admin1Pass, :location_id => 1
+
 admin1.approved = true
 admin1.superadmin = true
 admin1.save!
